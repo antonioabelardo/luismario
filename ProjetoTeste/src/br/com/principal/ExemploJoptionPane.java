3a3f273ce0d319619dcd799501a6665633c5ff1c
antonioabelardo <abelardo850309@gmail.com>
2019-03-21 14:35:21 -0300

    commit teste.

---
+++ ProjetoTeste/src/br/com/principal/ExemploJoptionPane.java
@@ -0,0 +1,13 @@
+package br.com.principal;
+
+import javax.swing.JOptionPane;
+
+public class ExemploJoptionPane {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Saida com JOptionPane.");
+
+	}
+
+}
---
+++ ProjetoTeste/src/br/com/principal/ExemploMain.java
@@ -0,0 +1,12 @@
+package br.com.principal;
+
+public class ExemploMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Teste saida via System.out.");
+
+	}
+
+}
